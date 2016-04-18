dd1a4af0b5e1bde6a5ec5ad5e029effaa1e0d160
Tenzing Brady <zingten@zingalicio.us>
2016-04-18 14:26:55 -0700

    Tabs look bad in CloudPebble

--- src/vibes.c
+++ src/vibes.c
@@ -2,28 +2,28 @@
 
 static const uint32_t vibe_connect_data[] = {50, 50, 50};
 static VibePattern vibe_connect = {
-	.durations = vibe_connect_data,
-	.num_segments = 3
+    .durations = vibe_connect_data,
+    .num_segments = 3
 };
 static const uint32_t vibe_disconnect_data[] = {50, 50, 50, 50, 50};
 static VibePattern vibe_disconnect = {
-	.durations = vibe_disconnect_data,
-	.num_segments = 5
+    .durations = vibe_disconnect_data,
+    .num_segments = 5
 };
-static const uint32_t vibe_quarter_data[] = {100, 250, 300, 250, 100, 250, 100};
+static const uint32_t vibe_quarter_data[] = {500, 250, 100};
 static VibePattern vibe_quarter = {
-	.durations = vibe_quarter_data,
-	.num_segments = 7
+    .durations = vibe_quarter_data,
+    .num_segments = 3
 };
-static const uint32_t vibe_half_data[] = {100, 250, 100, 250, 300, 250, 100};
+static const uint32_t vibe_half_data[] = {100, 250, 500};
 static VibePattern vibe_half = {
-	.durations = vibe_half_data,
-	.num_segments = 7
+    .durations = vibe_half_data,
+    .num_segments = 3
 };
-static const uint32_t vibe_threequarter_data[] = {100, 250, 100, 250, 100, 250, 300};
+static const uint32_t vibe_threequarter_data[] = {500, 250, 500};
 static VibePattern vibe_threequarter = {
-	.durations = vibe_threequarter_data,
-	.num_segments = 7
+    .durations = vibe_threequarter_data,
+    .num_segments = 3
 };
 
 #define VIBE_HOUR_ONE 2
@@ -34,6 +34,6 @@ static VibePattern vibe_threequarter = {
 #define VIBE_HOUR_SHORT 100
 static uint32_t vibe_hour_data[] = {1000, 1000, VIBE_HOUR_SHORT, 350, VIBE_HOUR_SHORT, 350, VIBE_HOUR_SHORT, 350, VIBE_HOUR_SHORT};
 static VibePattern vibe_hour = {
-	.durations = vibe_hour_data,
-	.num_segments = 9
+    .durations = vibe_hour_data,
+    .num_segments = 9
 };
